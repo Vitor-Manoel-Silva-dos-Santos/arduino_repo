5bd962d86b1d33dd1992f58211f94f3663748397
Vitor <vitorm2711@gmail.com>
2022-09-10 12:16:24 -0300

    Added initial structure

--- arduino.c
+++ arduino.c
@@ -4,13 +4,34 @@
 #define VENTILADOR 2
 dht DHT11;
 
+int vJanelaLig = 1;
+int vUmidificadorLig = 1;
+int vVentiladorLig = 1;
+int vDespertadorLig = 1;
+int vIluminacaoLig = 1;
+
+void mJanela();
+void mUmidificador();
+void mVentilador();
+void mDespertador();
+void mIluminacao();
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(VENTILADOR, OUTPUT);
 }
+
 void loop()
 {
+  if (vJanelaLig) mJanela();
+  if (vUmidificadorLig) mUmidificador();
+  if (vVentiladorLig) mVentilador();
+  if (vDespertadorLig) mDespertador();
+  if (vIluminacaoLig) mIluminacao();
+}
+
+void mVentilador() {
   Serial.println();
   DHT11.read11(DHT11PIN);
   float vTemperaturaDentro;
@@ -29,5 +50,4 @@ void loop()
   else{
      digitalWrite(VENTILADOR,LOW);
   }
-
 }
