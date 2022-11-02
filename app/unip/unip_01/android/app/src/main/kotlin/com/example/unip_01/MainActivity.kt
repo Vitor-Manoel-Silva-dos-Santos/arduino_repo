1142a9f95e65b1edc2c5243889958410fe9e17f8
Josué Teobaldo <josueteobaldo220@hotmail.com>
2022-11-02 16:45:02 -0300

    commit APP!!!!

--- app/unip/unip_01/android/app/src/main/kotlin/com/example/unip_01/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.unip_01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app/unip/unip_01/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'pagina_principal.dart';
-
-void main() => runApp(MaterialApp(home: HomePage()));
