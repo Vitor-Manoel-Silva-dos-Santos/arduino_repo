1c156c0ca61eebbb81772e3908a40e81d537b607
Josué Teobaldo dos Santos <josueteobaldo220@hotmail.com>
2022-11-15 23:10:02 -0300

    commit

--- app/aplicativo/lib/app_pages/iluminacao.dart
+++ app/aplicativo/lib/app_pages/iluminacao.dart
@@ -21,7 +21,7 @@ class _iluminacaoState extends State<iluminacao> {
       backgroundColor: const Color.fromARGB(255, 1, 2, 19),
       body: Container(
         padding: const EdgeInsets.all(10),
-        alignment: Alignment.topCenter,
+        alignment: Alignment.topLeft,
         decoration: const BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
@@ -30,19 +30,47 @@ class _iluminacaoState extends State<iluminacao> {
               Color.fromARGB(255, 1, 2, 19),
               Color.fromARGB(255, 3, 3, 39),
             ])),
-        child: Column(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Cor e Iluminação Atual",
-            style: TextStyle(color: Colors.white, fontSize: 20),),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(40)),
-              ),
-              width: 300,
-              height: 66,
-            ),
+            Column(
+              children: [
+                const Text(
+                  "Cor e Iluminação Atual",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        spreadRadius: 2,
+                        blurRadius: 4,
+                        offset: Offset(2, 3),
+                        color: Color.fromARGB(187, 9, 98, 146),
+                      )
+                    ],
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color.fromARGB(255, 8, 12, 87),
+                          Color.fromARGB(225, 12, 12, 143),
+                        ]),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  width: 360,
+                  height: 60,
+                  child: Row(
+                    children: [
+                      Container(
+                      ),
+                      Image(image: AssetImage('images/color-circle.png'),)
+                    ],
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
